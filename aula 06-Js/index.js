d7ad930a222b34eaca1ae66757b2927ce177091a
Kevyn <dosreiskevyn@gmail.com>
2024-08-05 21:24:31 -0300

    aprendendo let e const

---
+++ aula 06-Js/index.js
@@ -0,0 +1,16 @@
+let nome; //declarou a variavel 
+nome = 'Marcos'; //atribuiu o valor a variavel e inicializou
+console.log(nome);
+nome = 'Evan' //reatribui outro valor a variavel
+console.log(nome);
+
+
+
+//Não podemos criar variaveis com palavras reservadas ex: (let, console, for)
+//Variaveis precisam ter nomes significativos ex: (nameClient, emailClient)
+//Não podemos começar uma variavel com número ex: (1ano, 3carrinhos)
+//Variaveis não podem conter traços ou espaço ex: (nome-user, nome user)
+//Utilizamos camelCase ex:(primaryClient, secondaryClient)
+//As variaveis são case sensitive  ex: (A é diferente de a; Maria é diferente de maria; nome diferente de Nome)
+//Não podemos redeclarar variaveis com let
+//Não utilize LET, sempre o LET
---
+++ aula 07-Js/const.js
@@ -0,0 +1,8 @@
+const primeiroNumero = "6";
+const segundoNumero = 5;
+const resultadoNumeros = primeiroNumero * segundoNumeoro;
+const exibir = resultadoNumeros * 2;
+let resultadoTriplicado = resultadoNumeros * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(primeiroNumero + segundoNumeoro);
