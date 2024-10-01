d70078448e9b2b9f862bef439a5dec45e39e3704
Kevyn <dosreiskevyn@gmail.com>
2024-09-30 21:21:14 -0300

    aprendendo sobre atributos Math

---
+++ aula 15-Js/script.js
@@ -0,0 +1,8 @@
+let num1 = 9.49;
+//console.log(Math.floor(num1));
+//console.log(Math.ceil(num1));
+//console.log(Math.round(num1));
+//console.log(Math.max(1,2,5,8,800,200,3000,-5,-7,-5000));
+//console.log(Math.min(1,2,5,8,800,200,3000,-5,-7,-5000));
+//let aleatorio = Math.round(Math.random() * (10 - 4) + 4);
+console.log(Math.PI);
