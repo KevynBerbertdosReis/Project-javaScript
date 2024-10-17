ca7b6a1b13981b9bece679d661f122ed52d703ac
Kevyn <dosreiskevyn@gmail.com>
2024-10-16 21:59:38 -0300

    Aulas de JavaScript

--- aula 15-Js/script.js
+++ aula 15-Js/script.js
@@ -5,4 +5,4 @@ let num1 = 9.49;
 //console.log(Math.max(1,2,5,8,800,200,3000,-5,-7,-5000));
 //console.log(Math.min(1,2,5,8,800,200,3000,-5,-7,-5000));
 //let aleatorio = Math.round(Math.random() * (10 - 4) + 4);
-console.log(Math.PI);
+//console.log(Math.PI);
