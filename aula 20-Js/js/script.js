3745cc3ed0134de7e75f180ef0231c809bf2d3fc
Kevyn <dosreiskevyn@gmail.com>
2024-10-17 00:32:53 -0300

    Exercicios de Arrays e Objetos

--- aula 20-Js/js/script.js
+++ aula 20-Js/js/script.js
@@ -1,1 +1,28 @@
-alert(1);
+function meuEscopo () {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    function eventoForm(evento) {
+        evento.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+    };
+    console.log(pessoas);
+    
+
+    form.addEventListener('submit', eventoForm);
+};
+
+meuEscopo();
